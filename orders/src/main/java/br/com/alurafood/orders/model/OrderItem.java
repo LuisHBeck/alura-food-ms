a9ef80aca535132ccc948f90f565bf89ba1a58f9
LuisHBeck <beck.henrique.luis@gmail.com>
2023-12-31 18:55:58 -0300

    fix: id generation type to identity

--- orders/src/main/java/br/com/alurafood/orders/model/OrderItem.java
+++ orders/src/main/java/br/com/alurafood/orders/model/OrderItem.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class OrderItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Integer amount;
