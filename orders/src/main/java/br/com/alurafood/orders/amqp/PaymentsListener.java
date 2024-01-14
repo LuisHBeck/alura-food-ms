de3de3ee18b64f4b75428b1f519920e8f0d3d4a7
LuisHBeck <beck.henrique.luis@gmail.com>
2024-01-14 18:18:52 -0300

    feat: limiting attempts to process the message from RabbitMQ

--- orders/src/main/java/br/com/alurafood/orders/amqp/PaymentsListener.java
+++ orders/src/main/java/br/com/alurafood/orders/amqp/PaymentsListener.java
@@ -11,6 +11,14 @@ public class PaymentsListener {
 //    @RabbitListener(queues = "payment.finished") payment queue
     @RabbitListener(queues = "payments.order-detail") // order create the payment queue binded to the exchange
     public void receiveMessage(PaymentDto payment) {
+
+        System.out.println(payment.id());
+        System.out.println(payment.number());
+
+        if(payment.number().equals("0")) {
+            throw new RuntimeException("CANT PROCESS THE PAYMENT!");
+        }
+
         String message = String.format("""
                 Payment data: %s
                 Order number: %s
